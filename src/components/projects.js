de716b03bacafc3dc94dde1e09ee64a0f578bc4e
Ben Abbott <babbott85@gmail.com>
2024-02-05 13:03:40 -0700

    work cards figured out

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,62 +1,73 @@
 import React from "react"
 import {Container, Row, Col, Card, CardImg, CardImgOverlay, CardText, CardTitle} from 'reactstrap'
 import 'bootstrap/dist/css/bootstrap.css'
+import Mod from '../assets/modps.jpg'
+import Summit from '../assets/summitps.jpg'
 function Projects (){
     return(
         <>
         <Container>
-        <Row xs="2">
+        <Row lg="3">
             <Col>
         <div>
+            <div className="testing">
   <Card inverse>
     <CardImg
       alt="Card image cap"
-      src="https://picsum.photos/900/270?grayscale"
+      src={Mod}
       style={{
-        height: 270
+        height: 270,
+        // opacity: 0.4
       }}
     //   width="100%"
     />
     <CardImgOverlay>
-      <CardTitle tag="h5">
+      {/* <CardTitle tag="h5" className="ctitle"
+     
+      >
         Card Title
-      </CardTitle>
-      <CardText>
-        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+      </CardTitle> */}
+      <div className="ctoverlay">
+      <CardText className="ctitle">
+      <h3>Mod Properties</h3>
       </CardText>
-      <CardText>
+      <CardText className="ctitle">
         <small className="text-muted">
-          Last updated 3 mins ago
+          <a href="https://modprop.info/" target='_blank' rel="noreferrer"> <button className='sitebtn'>Visit Site</button></a>
         </small>
       </CardText>
+      </div>
     </CardImgOverlay>
   </Card>
- 
+ </div>
 </div>
 </Col>
+<Col></Col>
 <Col >
-        <div>
+        <div className="testing2">
   <Card inverse>
     <CardImg
       alt="Card image cap"
-      src="https://picsum.photos/900/270?grayscale"
+      src={Summit}
       style={{
         height: 270
       }}
     //   width="100%"
     />
     <CardImgOverlay>
-      <CardTitle tag="h5">
+      {/* <CardTitle tag="h5">
         Card Title
-      </CardTitle>
+      </CardTitle> */}
+      <div className="ctoverlay">
       <CardText>
-        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+        <h3>Summit Junior Tour</h3>
       </CardText>
       <CardText>
         <small className="text-muted">
-          Last updated 3 mins ago
+        <a href="https://summitjuniortour.com/" target='_blank' rel="noreferrer"> <button className='sitebtn'>Visit Site</button></a>
         </small>
       </CardText>
+      </div>
     </CardImgOverlay>
   </Card>
  
